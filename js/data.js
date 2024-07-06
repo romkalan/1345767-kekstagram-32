9ff4e996161ed4281434aae0ecf27edca05bc62c
romkalan <romkalan@mail.ru>
2024-07-06 15:28:56 +0300

    create module structure for js code

---
+++ js/data.js
@@ -0,0 +1,64 @@
+import {getRandomInteger, getRandomArrayElement, checkUniqValues} from './util.js';
+
+const names = [
+  'Ivan Fedorov',
+  'Frank Sinatra',
+  'Vladimir Valdo',
+  'Kent Suwert',
+  'Boris Eltsin',
+  'Joe Trump',
+];
+
+const descriptions = [
+  'Сомнительно, но оукэй',
+  'Ты же наш, почему такой выродок?',
+  'Я реально прихерел',
+  'Простите меня грешника',
+  'Попробую объяснить',
+  'Ну сколько можно?',
+  'Мне похер, я так вижу',
+  'Какое-то величие, какая-то херня',
+  'Все мы виноваты в этом пиздеце',
+  'Я это не понимаю, мне это не интересно',
+];
+
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const uniqValuesTo999999 = [];
+const uniqValuesTo25 = [];
+
+const createComment = () => {
+  const id = checkUniqValues(0, 999999, uniqValuesTo999999);
+
+  return {
+    id,
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: getRandomArrayElement(messages),
+    name: getRandomArrayElement(names),
+  };
+};
+
+const createCommentsArray = () => Array.from({length: getRandomInteger(0, 30)}, createComment);
+
+const createPhotoDesription = () => {
+  const id = checkUniqValues(1, 25, uniqValuesTo25);
+
+  return {
+    id,
+    url: `photo/${getRandomInteger(1, 25)}.jpg`,
+    description: getRandomArrayElement(descriptions),
+    likes: getRandomInteger(15, 200),
+    comments: createCommentsArray(),
+  };
+};
+
+const createPhotoDescriptions = () => Array.from({length: 25}, createPhotoDesription);
+
+export {createPhotoDescriptions};
