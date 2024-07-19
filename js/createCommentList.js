d1f97046a538bb5897b341f4b65e09ce6cecc3d0
romkalan <romkalan@mail.ru>
2024-07-19 20:18:41 +0300

    add createCommentList from template module

---
+++ js/createCommentList.js
@@ -0,0 +1,30 @@
+const commentList = document.querySelector('.social__comments');
+const commentTemplate = document.querySelector('#comment')
+  .content
+  .querySelector('.social__comment');
+
+const createComment = ({avatar, message, name}) => {
+  const commentElement = commentTemplate.cloneNode(true);
+  commentElement.querySelector('.social__picture').src = avatar;
+  commentElement.querySelector('.social__picture').alt = name;
+  commentElement.querySelector('.social__text').textContent = message;
+
+  return commentElement;
+};
+
+const createCommentsFragment = (comments) => {
+  const similarListFragment = document.createDocumentFragment();
+
+  comments.forEach((comment) => {
+    const commentElement = createComment(comment);
+    similarListFragment.appendChild(commentElement);
+  });
+  return similarListFragment;
+};
+
+const renderComments = (comments) => {
+  commentList.innerHTML = '';
+  commentList.appendChild(createCommentsFragment(comments));
+};
+
+export {renderComments};
